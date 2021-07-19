1c24f179b2456de6604a7b66f2cdfbeb0ea8f979
accidentlywoo <accidentlywoo@gmail.com>
2021-07-19 11:16:06 +0900

    spring web socket

---
+++ demo/src/main/kotlin/com/ex/demo/controller/MessageController.kt
@@ -0,0 +1,21 @@
+package com.ex.demo.controller
+
+import com.ex.demo.dto.Message
+import com.ex.demo.dto.OutputMessage
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.stereotype.Controller
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Controller
+class MessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    fun send(message: Message): OutputMessage {
+        val time = SimpleDateFormat("HH:mm").format(Date())
+
+        return OutputMessage(message.from, message.text, time)
+    }
+}
---
+++ demo/src/main/kotlin/com/ex/demo/dto/Message.kt
@@ -0,0 +1,12 @@
+package com.ex.demo.dto
+
+class Message(
+    val from: String,
+    val text: String
+)
+
+class OutputMessage(
+    val from: String,
+    val text: String,
+    val time: String
+)
